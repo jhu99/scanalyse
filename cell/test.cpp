63ca99153cbd247e5b8c9f303e9e994c335a9530
ltwltw <30688258+ltwltw@users.noreply.github.com>
2018-10-09 11:49:08 +0800

    Update test.cpp

--- cell/test.cpp
+++ cell/test.cpp
@@ -1,12 +1,13 @@
 #include"cell.h"
 #include<iostream>
 using namespace std;
+
 int main() {
 	Cells cells(5005,4001);
-	cells.readFile();
+	cells.readFile("scimpute-0.01-1-data.csv");
 	//cells.findCell("AAACCTGAGCCACTAT.1");
 	cells.findGene("ENSMUSG00000037221");
-	//	cells.findCellAndGene("AAACCTGAGCCACTAT.1", "ENSMUSG00000015312");
+	//cells.findCellAndGene("AAACCTGAGCCACTAT.1", "ENSMUSG00000015312");
 	cin.get();
 	cin.get();
 	return 0;
