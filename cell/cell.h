5adebb4c1713036dd63d869f2f87a595d3edab54
ltwltw <30688258+ltwltw@users.noreply.github.com>
2018-10-10 21:59:34 +0800

    Update cell.h

--- cell/cell.h
+++ cell/cell.h
@@ -1,5 +1,5 @@
-#ifndef CELL_H
-#define CELL_H
+#ifndef CELLS_H
+#define CELLS_H
 
 #include<string>
 #include<unordered_map>
@@ -13,25 +13,26 @@ class Cells {
 	unordered_map<int, string> numToCell;
 	unordered_map<int, string> numToGene;
 public:
-	Cells() {
+	Cells();
+	Cells(int n, int p);
+	~Cells();
 
-	}
-	Cells(int nn,int pp):n(nn),p(pp){
-		cell = new unsigned short *[n];
-		for (int i = 0; i <= n; i++) {
-			cell[i] = new unsigned short[p];
-		}
-	}
-	~Cells() {
-		for (int i = 0; i <= n; i++) {
-			delete[] cell[i];
-		}
-		delete[] cell;
-	}
+	void setCell(unsigned short** cell);
+	void setCellToNUm(unordered_map<string, int> cellToNum);
+	void setGeneToNUm(unordered_map<string, int> geneToNum);
+	void setNumTocell(unordered_map<int, string> numToCell);
+	void setNumToGene(unordered_map<int, string> numToGene);
+
+	unsigned short **getCell();
+	unordered_map<string, int> getCellToNum();
+	unordered_map<string, int> getGeneToNum();
+	unordered_map<int, string> getNumToCell();
+	unordered_map<int, string> getNumToGene();
+
 	void readFile(string path);
 	void findCell(string cellName);
 	void findGene(string geneName);
 	void findCellAndGene(string cellName, string geneName);
 };
 
-#endif // !CELL_H
+#endif // !CELLS_H
