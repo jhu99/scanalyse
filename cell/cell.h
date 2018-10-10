b69eba5308da2a593cbc62c6792a37f949f50125
ltwltw <30688258+ltwltw@users.noreply.github.com>
2018-10-10 22:03:06 +0800

    Update cell.h

--- cell/cell.h
+++ cell/cell.h
@@ -16,14 +16,13 @@ public:
 	Cells();
 	Cells(int n, int p);
 	~Cells();
-
 	void setCell(unsigned short** cell);
 	void setCellToNUm(unordered_map<string, int> cellToNum);
 	void setGeneToNUm(unordered_map<string, int> geneToNum);
 	void setNumTocell(unordered_map<int, string> numToCell);
 	void setNumToGene(unordered_map<int, string> numToGene);
 
-	unsigned short **getCell();
+	unsigned short** getCell();
 	unordered_map<string, int> getCellToNum();
 	unordered_map<string, int> getGeneToNum();
 	unordered_map<int, string> getNumToCell();
