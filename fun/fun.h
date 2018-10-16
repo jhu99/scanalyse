81268bad61b0742a6d65094faae5ed48d70b4d63
ZYK <drizzlezyk@163.com>
2018-10-16 22:30:29 +0800

    Update fun/fun.h

--- fun/fun.h
+++ fun/fun.h
@@ -19,10 +19,10 @@ namespace Scanalyse
 		}
 		unordered_map<int, string> allNumToGene;
 		unordered_map<string, int> allGeneToNum;
-		vector<string> files;//存放文件夹中所有文件路径
-		int rowCount[100];//每个文件的行数
-		int columnCount[100];//每个文件的列数
-		vector<Cells> cellList;//存放每个文件的cells实例
+		vector<string> files;//store all paths of all files
+		int rowCount[100];//store row count of each file
+		int columnCount[100];//store column count of each file
+		vector<Cells> cellList;
 		unordered_map<string, string> geneSymbolToEnsemblId;
 		int mergeColumnCount=0;
 		dataType **mergeMatrix;
