6ebb1184e3e8e24f288fcca5111edb1e5a38489b
ZYK <drizzlezyk@163.com>
2018-10-20 15:05:58 +0800

    Update Tests/funTest.cpp

--- tests/funTest.cpp
+++ tests/funTest.cpp
@@ -1,26 +1,16 @@
 #include"fun.h"
 #include"cell/cell.cpp"
-#include <io.h>
 #include<iostream>
 #include <fstream>  
 #include <string>  
-#include <vector>  
 
 using namespace std;
 using namespace Scanalyse;
 
-
 int main()
-
 {
 	Fun fun;
 	fun = Fun();
-	/*
-	string path1 = "E:\\cell mapping\\GSE66507.csv";
-	string path2 = "E:\\cell mapping\\GSE66507mapping.csv";
-	fun.replaceEnsemblId(path1,path2);
-
-	*/
 	string filePath = "../../cell";
 	string outpath = "../../";
 	fun.read(filePath);
