18658a66e4a9e0d16568229ac45cbdbb22d78404
ltwltw <30688258+ltwltw@users.noreply.github.com>
2018-11-15 10:06:08 +0800

    Update linearRegressionParameter.cpp

--- lib/linearRegression/linearRegressionParameter.cpp
+++ lib/linearRegression/linearRegressionParameter.cpp
@@ -3,13 +3,18 @@
 LinearRegressionParameter::LinearRegressionParameter() {
 
 }
-LinearRegressionParameter::LinearRegressionParameter(double c0, double c1, double cov00, double cov01, double cov11, double sumsq) {
+LinearRegressionParameter::LinearRegressionParameter(double c0, double c1, double cov00, double cov01, double cov11, double sumsq, double sumtot, int n) {
 	this->c0 = c0;
 	this->c1 = c1;
 	this->cov00 = cov00;
 	this->cov01 = cov01;
 	this->cov11 = cov11;
 	this->sumsq = sumsq;
+	this->sumtot = sumtot;
+	this->n = n;
+	r2 = 1 - (sumsq / sumtot);
+	t = sqrt(r2 / (1 - r2)*(n - 2));
+	pvalue = gsl_cdf_tdist_Q(t, n - 1) * 2;
 
 }
 LinearRegressionParameter::~LinearRegressionParameter() {
@@ -34,6 +39,21 @@ void LinearRegressionParameter::setCov11(double cov11) {
 void LinearRegressionParameter::setSumsq(double sumsq) {
 	this->sumsq = sumsq;
 }
+void LinearRegressionParameter::setSumtot(double sumtot) {
+	this->sumtot = sumtot;
+}
+void LinearRegressionParameter::setN(int n) {
+	this->n = n;
+}
+void LinearRegressionParameter::setR2(double r2) {
+	this->r2 = r2;
+}
+void LinearRegressionParameter::setT(double t) {
+	this->t = t;
+}
+void LinearRegressionParameter::setPvalue(double pvalue) {
+	this->pvalue = pvalue;
+}
 
 double LinearRegressionParameter::getC0() {
 	return c0;
@@ -53,7 +73,32 @@ double LinearRegressionParameter::getCov11() {
 double LinearRegressionParameter::getSumsq() {
 	return sumsq;
 }
+double LinearRegressionParameter::getSumtot() {
+	return sumtot;
+}
+double LinearRegressionParameter::getN() {
+	return n;
+}
+double LinearRegressionParameter::getR2() {
+	return r2;
+}
+double LinearRegressionParameter::getT() {
+	return t;
+}
+double LinearRegressionParameter::getPvalue() {
+	return pvalue;
+}
 
 void LinearRegressionParameter::print() {
-	cout << c0 << " " << c1 << endl;
+	cout << "c0:" << c0 << endl;
+	cout << "c1:" << c1 << endl;
+	cout << "cov00:" << cov00 << endl;
+	cout << "cov01:" << cov01 << endl;
+	cout << "cov11:" << cov11<< endl;
+	cout << "sumsq:" << sumsq << endl;
+	cout << "sumtot:" << sumtot << endl;
+	cout << "n:" << n << endl;
+	cout << "r2:" << r2 << endl;
+	cout << "t:" << t << endl;
+	cout << "pvalue:" << pvalue << endl;
 }
