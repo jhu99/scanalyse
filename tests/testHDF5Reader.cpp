8ce7c99576a2f1cd231dd774d46f0ca776fd292f
ZYK <drizzlezyk@163.com>
2018-12-02 20:57:23 +0800

    Update testHDF5Reader.cpp

--- tests/testHDF5Reader.cpp
+++ tests/testHDF5Reader.cpp
@@ -10,6 +10,7 @@ int main(int argc, const char ** argv)
 	HDF5reader hr;
 	string path = argv[1];
 	hr.readHDF5File(path);
+	cout << "there are " << hr.get_gene_count() << " genes" << endl;
 	cout << "Before filtration, there are " << hr.get_cell_count() << " cells." << endl;
 	unordered_map<int,int>c=hr.cellFiltration();
 	cout <<"After filtration, there are " << hr.get_cell_count()-c.size()<<" cells left." << endl;
@@ -25,16 +26,7 @@ int main(int argc, const char ** argv)
 	char**a = hr.get_barcodes();
 	hr.createCellnameMap();
 	int* paraCellVector1=hr.createCellVectorByName(a[0]);
-	cout << "first cell" << endl;
-	for (int i = 33664; i >= 33653; i--)
-	{
-		cout << paraCellVector1[i]<<" ";
-	}
-	cout << endl;
-	cout <<"second cell"<< endl;
-	int* paraCellVector2 = hr.createCellVectorByName(a[1]);
-	cout << paraCellVector2[32919]<<" "<< paraCellVector2[31978] << " " << paraCellVector2[31763] << " " << paraCellVector2[31453] << " " << paraCellVector2[31365] << " " << paraCellVector2[30936] << " " << paraCellVector2[30740] << " " << paraCellVector2[30434] << " " << paraCellVector2[30420] << " " << paraCellVector2[29944];
-	cout << endl;
+
 	
 	cin.get();
 	cin.get();
