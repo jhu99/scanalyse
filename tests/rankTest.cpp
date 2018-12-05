538258cb26d90a449d2dff8bb80052ac58595896
ltwltw <30688258+ltwltw@users.noreply.github.com>
2018-12-05 20:05:54 +0800

    Update rankTest.cpp

--- tests/rankTest.cpp
+++ tests/rankTest.cpp
@@ -1,12 +1,20 @@
 #include<iostream>
 #include"HDF5Reader/HDF5Reader.h"
 #include"rank/rankNormalize.h"
+#include<ctime>
 using namespace std;
 
 int main() {
+	clock_t start,end;
 	HDF5reader hr;
-	hr.readHDF5File("C:/Users/ltw/Desktop/ica_cord_blood_h5.h5");
+	hr.readHDF5File("./data/ica_cord_blood_h5.h5");
+	start=clock();
 	rankNormalize rn(hr);
-	rn.ranks();
-	rn.print();
+	rn.ranks(5);
+	end=clock();
+	double endtime=(double)(end-start)/CLOCKS_PER_SEC;
+	//cout<<"Total time:"<<endtime<<endl;		
+	//rn.print();
+	cout<<"Total time:"<<endtime<<endl;		
+	hr.deleteHDF5();
 }
