d3ae066ee666254dc21b3ebf6c693b04582443fc
ltwltw <30688258+ltwltw@users.noreply.github.com>
2018-12-14 10:05:36 +0800

    Update geneExpressionTop.h

--- lib/geneTop/geneExpressionTop.h
+++ lib/geneTop/geneExpressionTop.h
@@ -5,19 +5,19 @@
 #include<algorithm>
 class geneExpressionTop {
 	int topNum = 0, num=0;
-	HDF5reader hr;
+	SparseMatrix sm;
 	geneCount * gc;
 	string *top;
 public:
-	geneExpressionTop(HDF5reader hr,int topNum);
+	geneExpressionTop(SparseMatrix sm,int topNum);
 	~geneExpressionTop();
-	void setHDF5reader(HDF5reader hr);
+	void setSparseMatrix(SparseMatrix sm);
 	void setTopNum(int topNum);
 	void setNum(int num);
 	void setGeneCount(geneCount *gc);
 	void setTop(string *top);
 
-	HDF5reader getHDF5reader();
+	HDF5reader getSparseMatrix();
 	int getTopNum();
 	int getNum();
 	geneCount* getGeneCount();
