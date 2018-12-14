fffe89c01e00bf158add61e0119f5697614c416c
ltwltw <30688258+ltwltw@users.noreply.github.com>
2018-12-14 10:45:35 +0800

    Update geneTopsTest.cpp

--- tests/geneTopsTest.cpp
+++ tests/geneTopsTest.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
 	SparseMatrix sm;
 	//freopen("C:/Users/ltw/Desktop/1.txt", "w", stdout);
-	string path = "C:/Users/ltw/Desktop/ica_cord_blood_h5.h5";
+	string path = "./data/ica_cord_blood_h5.h5";
 	sm.readHDF5File(path);
 	/*for (int i = 0; i < hr.get_gene_count(); i++) {
 		cout << i<<" "<<hr.get_genes()[i] << endl;
