4c584acb8e6817463af2af93830b0210f524bebd
screamer <jhu@nwpu.edu.cn>
2019-01-17 10:15:33 -0500

    other variables

--- code/main.py
+++ code/main.py
@@ -9,7 +9,7 @@ FMT=sys.argv[4]
 
 #Argv 1: Pathway of the input file
 if FUN=='train':
-	train.train_model($INPUT_FILE,$OUTPUT_FILE,$FMT)
+	train.train_model(INPUT_FILE,OUTPUT_FILE,FMT)
 #Argv 2: Pathway of the load_weight
 elif FUN=='prediction':
 	load_model.load_weight(sys.argv[1],sys.argv[2],sys.argv[3])
