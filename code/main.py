cf53f843691267580662437b01fdcebbfe64d26f
screamer <jhu@nwpu.edu.cn>
2019-01-17 10:00:58 -0500

    parameter control

--- code/main.py
+++ code/main.py
@@ -1,8 +1,16 @@
 import train
 import sys
 import load_model
-#Argv 1: Pathway of the input file
-#train.train_model(sys.argv[1],sys.argv[2],sys.argv[3])
+FUN = sys.argv[1]
+INPUT_FILE=sys.argv[2]
+OUTPUT_FILE=sys.argv[3]
+FMT=sys.argv[4]
 
+#Argv 1: Pathway of the input file
+if $FUN=='train':
+	train.train_model($INPUT_FILE,$OUTPUT_FILE,$FMT)
 #Argv 2: Pathway of the load_weight
-load_model.load_weight(sys.argv[1],sys.argv[2],sys.argv[3])
+elif $FUN=='prediction':
+	load_model.load_weight(sys.argv[1],sys.argv[2],sys.argv[3])
+else:
+	print "please use \'train\' or \'prediction\'"
