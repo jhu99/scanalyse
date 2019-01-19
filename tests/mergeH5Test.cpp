9b80ee26dda532a44a96a2ff7f6a43dace47ae70
ltwltw <30688258+ltwltw@users.noreply.github.com>
2019-01-19 13:28:57 +0800

    Create mergeH5Test.cpp

---
+++ tests/mergeH5Test.cpp
@@ -0,0 +1,17 @@
+#include"Merge.h"
+#include<vector>
+using namespace std;
+using namespace H5;
+
+int main() {
+	SparseMatrix m;
+	vector<string> paths;
+	string path = "../data/ica_cord_blood_h5.h5";
+	//string path = "D:/test.h5";
+	paths.push_back(path);
+	//paths.push_back(path);
+	m.mergeDate(paths);
+	m.h5Compressed("../data/test.h5", "s", 1000, 1);
+	getchar();
+	getchar();
+}
