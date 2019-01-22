95ea2affec596b784906f44c283bd8dc1fe97bea
ltwltw <30688258+ltwltw@users.noreply.github.com>
2019-01-23 00:09:12 +0800

    Update mergeH5Test.cpp

--- tests/mergeH5Test.cpp
+++ tests/mergeH5Test.cpp
@@ -6,9 +6,9 @@ using namespace H5;
 int main(int argc,const char** argv) {
 	SparseMatrix m;
 	vector<string> paths;
-	for(int i=1;i<argc;i++){
+	for(int i=2;i<argc;i++){
 	    paths.push_back(argv[i]);
 	}
 	m.mergeDate(paths);
-	m.h5Compressed("./data/test.h5", "s", 1000, 1);
+	m.h5Compressed(argv[1], "s", 1000, 1);
 }
