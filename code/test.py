6aa119dc428219f0b5c44eeb474df51fc7adccfc
screamer <jhu@nwpu.edu.cn>
2019-02-11 08:55:14 -0500

    Update test.py

--- code/test.py
+++ code/test.py
@@ -7,6 +7,7 @@ from getAnnData import getAnnData, getAnnData_10x_h5, getAnnData_10x_mtx
 def pre_process_input_data(gene_file,input_file,format_type="10x_mtx"):
 	if format_type == "10x_h5":
 		adata = getAnnData_10x_h5(input_file)
+		return adata
 	else:
 		adata = getAnnData_10x_mtx(input_file)
 	
@@ -30,5 +31,15 @@ def pre_process_input_data(gene_file,input_file,format_type="10x_mtx"):
 				dtype=X.dtype.name,
 				filemode=True)
 	return adata
+
+gene_file="../result/ica_all/input_gene_g1.csv"
+input_file1="../data/zheng/293t_filtered_gene_bc_matrices_mex/hg19/"
+input_file2="../data/geneFilterResult/293t_filtered_gene_bc_matrices_mex.h5"
+adata1=pre_process_input_data(gene_file,input_file1)
+adata2=pre_process_input_data(gene_file,input_file2,format_type="10x_h5")
+adata1.var
+adata2.var
+
+
 
 
