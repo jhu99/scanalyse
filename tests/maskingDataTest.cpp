afd6830accb1a6af065f6f00d29dd2b83fd696c6
ltwltw <2696867770@qq.com>
2019-02-11 20:02:31 -0800

    Update maskingData.cpp

--- tests/maskingDataTest.cpp
+++ tests/maskingDataTest.cpp
@@ -1,10 +1,10 @@
 #include"SparseMatrix/SparseMatrix.h"
-#include<vector>
+#include<cstdlib>
 using namespace std;
 using namespace H5;
 
 int main(int argc, const char ** argv) {
 	SparseMatrix m;
 	m.readHDF5File(argv[1], "original");
-	m.maskingData(argv[2], argv[3]);
+	m.maskingData(atoi(argv[2]), argv[3]);
 }
