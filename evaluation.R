929e9601ad69fd7e113860b0c2f1e4bcdc304499
Jialu Hu <jhu@nwpu.edu.cn>
2019-02-21 22:51:36 -0500

    mse evaluation

--- evaluation.R
+++ evaluation.R
@@ -1,22 +1,22 @@
 # R script for performance comparison
-library(readRDS)
 
 filenames_saverX=list.files("./result",recursive=T,pattern="rds",full.names=T)
 filenames_myres=list.files("./result",recursive=T,pattern="mean",full.names=T)
 filenames_raw=list.files("./data/geneFilterResult",pattern="csv",full.names=T)
 filenames_log=list.files("./data", recursive=T, pattern="293.*2_[0-9]{1,2}_log.csv", full.names=T)
-raw_x <- read.csv(filenames_raw,row.names=1)
+raw_x <- as.matrix(read.csv(filenames_raw,row.names=1))
 maskInd <- rep(0L,2000*3000)
 
+saverx_mse_samples<-rep(0,10)
+myres_mse_samples<-rep(0,10)
 for(i in 1:10)
 {
-	
 	fsaverx <- filenames_saverX[i]
 	fmyres <- filenames_myres[i]
 	flog <- filenames_log[i]
 	x1 <- readRDS(fsaverx)
-	x2 <- read.csv(fmyres,row.names=1)
-	ind <- read.csv(flog,header=F,col.names=paste('V',1:2000,sep=''))
+	x2 <- as.matrix(read.csv(fmyres,row.names=1))
+	ind <- as.matrix(read.csv(flog,header=F,col.names=paste('V',1:500,sep='')))
 	dimInd <- dim(ind)
 	rownum <- dimInd[1]
 	colnum <- dimInd[2]
@@ -35,13 +35,16 @@ for(i in 1:10)
 		}
 	}
 	
+	try(if(any(raw_x[maskIndValid]==0)) stop("Invalid log files!"))
+
 	maskIndValid = maskInd[maskInd>0]
-	mse1 <- mse(x1[maskIndValid]-rawx[maskIndValid])
-	mse2 <- mse(x2[maskIndValid]-rawx[maskIndValid])
+	saverx_mse_samples[i] <- mean((x1[maskIndValid]-raw_x[maskIndValid])^2)
+	myres_mse_samples[i] <- mean((x2[maskIndValid]-raw_x[maskIndValid])^2)
 	
 	maskInd[] <- 0
 	
 }
+
 
 
 
