e90d68c763a01d3aa0d7a3843c9a2279c828de13
ZYK <drizzlezyk@163.com>
2019-04-04 15:31:25 +0800

    Update garnett_mtx_arg.R

--- lib/Rlib/garnett_mtx_arg.R
+++ lib/Rlib/garnett_mtx_arg.R
@@ -1,7 +1,7 @@
 Args <- commandArgs()
 library(garnett)
-barcodes<-read.table(paste(Argvs[1],"barcodes.tsv",sep=""),header = F,sep="\t")
-genes<-read.table(paste(Argvs[1],"genes.tsv",sep=""),header = F,sep="\t")
+barcodes<-read.table(paste(Argvs[1],"barcodes.tsv",sep=""),header = F,sep="\t",row.names=1)
+genes<-read.table(paste(Argvs[1],"genes.tsv",sep=""),header = F,sep="\t",row.names=1)
 mat <- readMM(paste(Argvs[1],"matrix.mtx",sep=""))
 row.names(mat) <- row.names(genes)
 colnames(mat) <- row.names(barcodes)
@@ -10,3 +10,22 @@ fd <- new("AnnotatedDataFrame", data = genes)
 pbmc_cds <- newCellDataSet(as(mat, "dgCMatrix"),
                              phenoData = pd,
                              featureData = fd)
+pbmc_cds <- estimateSizeFactors(pbmc_cds)
+library(org.Hs.eg.db)
+marker_file_path <-"E:\\graduated\\data\\bones.txt"
+
+marker_check <- check_markers(pbmc_cds, marker_file_path,
+                              db=org.Hs.eg.db,
+                              cds_gene_id_type = "SYMBOL",
+                              marker_file_gene_id_type = "SYMBOL")
+head(pData(pbmc_cds))
+pbmc_classifier <- train_cell_classifier(cds = pbmc_cds,
+                                         marker_file = marker_file_path,
+                                         db=org.Hs.eg.db,
+                                         cds_gene_id_type = "SYMBOL",
+                                         num_unknown = 50,
+                                         marker_file_gene_id_type = "SYMBOL")
+feature_genes <- get_feature_genes(pbmc_classifier,
+                                   node = "root",
+                                   db = org.Hs.eg.db)
+head(feature_genes)
