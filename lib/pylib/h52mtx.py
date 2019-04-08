133e6911dbbe17046153814d4ed01e6b1ec8e767
ZYK <drizzlezyk@163.com>
2019-04-08 19:06:18 +0800

    Update h52mtx.py

--- lib/pylib/h52mtx.py
+++ lib/pylib/h52mtx.py
@@ -1,19 +1,22 @@
-from scipy.sparse import coo_matrix
 from scipy.sparse import csr_matrix
+from scipy.io import mmwrite
 from anndata import h5py
+import numpy as np
+
 
 class MatrixCSR():
     def __init__(self):
-        self.shape=None
-        self.indptr=None
-        self.indices=None
-        self.barcodes=None
-        self.genes=None
-        self.data=None
-        self.coo=None
-        self.gene_names=None
+        self.shape = None
+        self.indptr = None
+        self.indices = None
+        self.barcodes = None
+        self.genes = None
+        self.data = None
+        self.coo = None
+        self.gene_names = None
+        self.trans_m=None
 
-    def read_h5(self,input_file):
+    def read_h5(self, input_file):
         h5 = h5py.File(input_file, 'r')
         self.data = h5['/GRCh38/data']
         self.indices = h5['/GRCh38/indices']
@@ -21,12 +24,9 @@ class MatrixCSR():
         self.genes = h5['/GRCh38/genes']
         self.barcodes = h5['/GRCh38/barcodes']
         self.shape = h5['/GRCh38/shape']
-        self.gene_names=h5['/GRCh38/gene_names']
-        csr_m = csr_matrix((self.data, self.indices, self.indptr), shape=(self.shape[1],self.shape[0]))
-        self.coo = csr_m.tocoo()
-        print(self.coo.col[0])
-        print(self.barcodes[0])
-        print(self.genes[0])
+        self.gene_names = h5['/GRCh38/gene_names']
+        csr_m = csr_matrix((self.data, self.indices, self.indptr), shape=(self.shape[1], self.shape[0]))
+        self.trans_m=np.transpose(csr_m)
 
     def write2mtx(self, write_path):
         barcodes_path = write_path+'barcodes.tsv'
@@ -50,27 +50,8 @@ class MatrixCSR():
         file.close()
         print("genes writed")
         print(self.shape[0])
-        data_path=write_path+'matrix.mtx'
-        file = open(data_path, 'w')
-        file.write('%%MatrixMarket matrix coordinate real general\n')
-        file.write('%\n')
-        file.write(str(self.shape[0]))
-        file.write('\t')
-        file.write(str(self.shape[1]))
-        file.write('\t')
-        file.write(str(len(self.data)))
-        print(len(self.data))
-        print(len(self.coo.col))
-        file.write(' \n')
-        for i in range(len(self.coo.col)):
-            file.write(str(self.coo.col[i]+1))
-            file.write(' ')
-            file.write(str(self.coo.row[i]+1))
-            file.write(' ')
-            file.write(str(self.coo.data[i]))
-            file.write(' ')
-            file.write('\n')
-        file.close()
+        data_path = write_path + 'matrix.mtx'
+        mmwrite(data_path,self.trans_m)
         print("data writed")
 
 def convert_h52mtx(input_file,write_path):
