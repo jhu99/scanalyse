f4448589bf584ebd8cca6220775b4dba128cd4df
ZYK <drizzlezyk@163.com>
2019-04-09 13:15:09 +0800

    Update garnett_mtx_arg.R

--- lib/Rlib/garnett_mtx_arg.R
+++ lib/Rlib/garnett_mtx_arg.R
@@ -1,8 +1,9 @@
-Args <- commandArgs()
+Argvs<- commandArgs()
 library(garnett)
-barcodes<-read.table(paste(Argvs[1],"barcodes.tsv",sep=""),header = F,sep="\t",row.names=1)
-genes<-read.table(paste(Argvs[1],"genes.tsv",sep=""),header = F,sep="\t",row.names=1)
-mat <- readMM(paste(Argvs[1],"matrix.mtx",sep=""))
+print(paste(Argvs[6],"barcodes.tsv",sep=""))
+barcodes<-read.table(paste(Argvs[6],"barcodes.tsv",sep=""),header = F,sep="\t",row.names=1)
+genes<-read.table(paste(Argvs[6],"genes.tsv",sep=""),header = F,sep="\t",row.names=1)
+mat <- readMM(paste(Argvs[6],"matrix.mtx",sep=""))
 row.names(mat) <- row.names(genes)
 colnames(mat) <- row.names(barcodes)
 pd <- new("AnnotatedDataFrame", data = barcodes)
@@ -12,20 +13,29 @@ pbmc_cds <- newCellDataSet(as(mat, "dgCMatrix"),
                              featureData = fd)
 pbmc_cds <- estimateSizeFactors(pbmc_cds)
 library(org.Hs.eg.db)
-marker_file_path <-"E:\\graduated\\data\\bones.txt"
+marker_file_path <-Argvs[7]
 
 marker_check <- check_markers(pbmc_cds, marker_file_path,
                               db=org.Hs.eg.db,
-                              cds_gene_id_type = "SYMBOL",
+                              cds_gene_id_type = "ENSEMBL",
                               marker_file_gene_id_type = "SYMBOL")
 head(pData(pbmc_cds))
 pbmc_classifier <- train_cell_classifier(cds = pbmc_cds,
                                          marker_file = marker_file_path,
                                          db=org.Hs.eg.db,
-                                         cds_gene_id_type = "SYMBOL",
+                                         cds_gene_id_type = "ENSEMBL",
                                          num_unknown = 50,
                                          marker_file_gene_id_type = "SYMBOL")
 feature_genes <- get_feature_genes(pbmc_classifier,
                                    node = "root",
                                    db = org.Hs.eg.db)
 head(feature_genes)
+pbmc_cds <- classify_cells(pbmc_cds, pbmc_classifier,
+                           db = org.Hs.eg.db,
+                           cluster_extend = TRUE,
+                           cds_gene_id_type = "ENSEMBL")
+head(pData(pbmc_cds))
+type<-pData(pbmc_cds)$cell_type
+write.table(type,Argvs[8],row.names = FALSE,col.names = FALSE)
+
+
