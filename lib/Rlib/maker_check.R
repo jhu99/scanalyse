6bf075e98b2508c53177e2534d72406949620ac4
ZYK <drizzlezyk@163.com>
2019-04-11 10:25:52 +0800

    Create maker_check.R

---
+++ lib/Rlib/maker_check.R
@@ -0,0 +1,22 @@
+Argvs<- commandArgs()
+library(garnett)
+print(paste(Argvs[6],"barcodes.tsv",sep=""))
+barcodes<-read.table(paste(Argvs[6],"barcodes.tsv",sep=""),header = F,sep="\t",row.names=1)
+genes<-read.table(paste(Argvs[6],"genes.tsv",sep=""),header = F,sep="\t",row.names=1)
+mat <- readMM(paste(Argvs[6],"matrix.mtx",sep=""))
+row.names(mat) <- row.names(genes)
+colnames(mat) <- row.names(barcodes)
+pd <- new("AnnotatedDataFrame", data = barcodes)
+fd <- new("AnnotatedDataFrame", data = genes)
+pbmc_cds <- newCellDataSet(as(mat, "dgCMatrix"),
+                             phenoData = pd,
+                             featureData = fd)
+pbmc_cds <- estimateSizeFactors(pbmc_cds)
+library(org.Hs.eg.db)
+marker_file_path <-Argvs[7]
+
+marker_check <- check_markers(pbmc_cds, marker_file_path,
+                              db=org.Hs.eg.db,
+                              cds_gene_id_type = "ENSEMBL",
+                              marker_file_gene_id_type = "SYMBOL")
+write.csv(marker_check,Argvs[8])
